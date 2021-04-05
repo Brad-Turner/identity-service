3278a1ad2a8ed1c5cbd7a063fb2731d324cc7e4f
Brad Turner <dev.bradturner@gmail.com>
2021-04-05 11:31:11 +1000

    Add github workflow for heroku deployment
    Adjusted .dockerignore file to include migrations

--- src/app.ts
+++ src/app.ts
@@ -5,10 +5,10 @@ import DB from './db';
 import { attachRoutes } from './routes';
 
 const logger = Logger();
-const DEFAULT_DB_CONNECTION_STRING = 'postgresql://127.0.0.1:5432';
+const DEFAULT_DATABASE_URL = 'postgresql://127.0.0.1:5432';
 
 export async function setupApp(): Promise<Express> {
-  const connectionString = process.env.DB_CONNECTION_STRING ?? DEFAULT_DB_CONNECTION_STRING;
+  const connectionString = process.env.DATABASE_URL ?? DEFAULT_DATABASE_URL;
 
   DB.connect({ connectionString }); // Initialises pooling
 
